f9c84c9eac511db7e72e88ae0aa66e924476e4bf
Priyanshi Chauhan <priyanshichauhan0707@gmail.com>
2022-09-07 20:56:10 +0530

    efs-ola

--- output.tf
+++ output.tf
@@ -9,11 +9,3 @@ output "efs_file_system_id" {
 output "efs_file_system_dns" {
   value = aws_efs_file_system.efs_file_system.dns_name
 }
-
-output "efs_access_point_id" {
-  value = aws_efs_access_point.efs_access.id
-}
-
-output "efs_access_point_arn" {
-  value = aws_efs_access_point.efs_access.arn
-}
---
+++ var.tf
@@ -0,0 +1,34 @@
+variable "efs_name" {
+  description = "Name of the EFS"
+  type        = string
+  default     = "test"
+}
+
+variable "creation_token_name" {
+  description = "A unique name used as reference when creating the Elastic File System to ensure idempotent file system creation."
+  type        = string
+  default     = "efs"
+}
+
+variable "availability_zone" {
+  description = "It will the efs on the specific zone."
+  type        = string
+  default     = "us-east-2b"
+}
+
+
+variable "additional_tags" {
+  description = "Additional tags for the efs"
+  type        = map(any)
+  default     = {}
+}
+
+variable "mount_target_subnet_id" {
+  description = "The ID of the subnet to add the mount target in"
+  type        = string
+  default     = "subnet-0f00ab85e6ce24f7e"
+}
+
+
+
+
